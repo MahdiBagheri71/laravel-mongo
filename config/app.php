ea84d70fa77d55a8d2bd8bcd0b6397c203121c7d
mahdi <MahdiBagheriVar@gmail.com>
2023-12-25 16:02:41 +0330

    feat: install mongodb/laravel-mongodb

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+
+        //MONGO
+        MongoDB\Laravel\MongoDBServiceProvider::class,
     ])->toArray(),
 
     /*
